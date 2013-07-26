47519b32d49f70d67681e0044fa1c3486fbfef4c
jamie.cook <jamie.cook@1d98dbbe-d728-0410-a43a-8c3833854064>
2013-07-26 03:46:23 +0000

    - tidy up

--- spec/ScheduleTestProject/jobs/Tests/Transfer/tc_fares.rb
+++ spec/ScheduleTestProject/jobs/Tests/Transfer/tc_fares.rb
@@ -23,75 +23,70 @@ class TC_fares < OtTestCase
   def check_load_based_on_usage(usage)
     assert_in_delta(10,       @db.get_value('link5_2data1', [1,1,'Walk',10050,1,1,1,1,0], "load"), TEST_DELTA, "walk access")
     assert_in_delta(10,       @db.get_value('link5_2data1', [2,1,'PT',  10060,1,1,1,1,3], "load"), TEST_DELTA, "single transit line")
-    assert_in_delta(usage[0], @db.get_value('link5_2data1', [6,1,'Bus',10070,1,1,1,1,4],  "load"), TEST_DELTA, "optional transit line 1")
-    assert_in_delta(usage[0], @db.get_value('link5_2data1', [7,1,'Bus',10072,1,1,1,2,4],  "load"), TEST_DELTA, "optional transit line 1")
-    assert_in_delta(usage[0], @db.get_value('link5_2data1', [4,1,'Bus',10076,1,1,1,1,4],  "load"), TEST_DELTA, "optional transit line 1")
-    assert_in_delta(usage[1], @db.get_value('link5_2data1', [6,1,'PT',10075,1,1,1,1,5],   "load"), TEST_DELTA, "optional transit line 2")
-    assert_in_delta(usage[1], @db.get_value('link5_2data1', [7,1,'PT',10076,1,1,1,2,5],   "load"), TEST_DELTA, "optional transit line 2")
-    assert_in_delta(usage[1], @db.get_value('link5_2data1', [4,1,'PT',10077,1,1,1,1,5],   "load"), TEST_DELTA, "optional transit line 2")
-    assert_in_delta(usage[0], @db.get_value('link5_2data1', [5,1,'Walk',10080,1,1,1,2,0], "load"), TEST_DELTA, "walk egress from line 1")
-    assert_in_delta(usage[1], @db.get_value('link5_2data1', [5,1,'Walk',10078,1,1,1,2,0], "load"), TEST_DELTA, "walk egress from line 2")
+
+    assert_in_delta(usage[0], @db.get_value('link5_2data1', [6,1,'Bus',10070,1,1,1,1,4],  "load"), TEST_DELTA, "optional transit line 4")
+    assert_in_delta(usage[0], @db.get_value('link5_2data1', [7,1,'Bus',10073,1,1,1,2,4],  "load"), TEST_DELTA, "optional transit line 4")
+    assert_in_delta(usage[0], @db.get_value('link5_2data1', [4,1,'Bus',10076,1,1,1,1,4],  "load"), TEST_DELTA, "optional transit line 4")
+
+    assert_in_delta(usage[1], @db.get_value('link5_2data1', [6,1,'PT',10075,1,1,1,1,5],   "load"), TEST_DELTA, "optional transit line 5")
+    assert_in_delta(usage[1], @db.get_value('link5_2data1', [7,1,'PT',10076,1,1,1,2,5],   "load"), TEST_DELTA, "optional transit line 5")
+    assert_in_delta(usage[1], @db.get_value('link5_2data1', [4,1,'PT',10077,1,1,1,1,5],   "load"), TEST_DELTA, "optional transit line 5")
+
+    assert_in_delta(usage[0], @db.get_value('link5_2data1', [5,1,'Walk',10080,1,1,1,2,0], "load"), TEST_DELTA, "walk egress from line 4")
+    assert_in_delta(usage[1], @db.get_value('link5_2data1', [5,1,'Walk',10078,1,1,1,2,0], "load"), TEST_DELTA, "walk egress from line 5")
   end
 
   def test_simple
     # define the fare systems
     set_fare_system({4 => 1, 5 => 2}) # 1 => distance based, 2 => time based
 
-    assert_nothing_raised() {
 
-      # schedule based properties
-      @tr.scheduleStartTime = @timePeriods.first
-      @tr.scheduleDurations = [5]
-      @tr.logitParameters  = [0.1]
+    # schedule based properties
+    @tr.scheduleStartTime = @timePeriods.first
+    @tr.scheduleDurations = [5]
+    @tr.logitParameters  = [0.1]
 
-      # EXECUTE!!
-      @tr.execute
+    # EXECUTE!!
+    @tr.execute
 
-      # the different fare systems used by the two competing paths should equalise their total costs... 50/50 load
-      check_load_based_on_usage([5,5])
-    }
+    # the different fare systems used by the two competing paths should equalise their total costs... 50/50 load
+    check_load_based_on_usage([5,5])
   end
 
   def test_route_factors
     # define the fare systems
     set_fare_system({4 => 1, 5 => 2}) # 1 => distance based, 2 => time based
 
-    assert_nothing_raised() {
+    # schedule based properties
+    @tr.scheduleStartTime = @timePeriods.first
+    @tr.scheduleDurations = [5]
+    @tr.logitParameters   = [0.1]
+    @tr.routeFactors      = [[40, 0, 60], [[30,31,32], 0, 60, 60, 60, 2]] # double fares
 
-      # schedule based properties
-      @tr.scheduleStartTime = @timePeriods.first
-      @tr.scheduleDurations = [5]
-      @tr.logitParameters   = [0.1]
-      @tr.routeFactors      = [[40, 0, 60], [[30,31,32], 0, 60, 60, 60, 2]] # double fares
+    # EXECUTE!!
+    @tr.execute
 
-      # EXECUTE!!
-      @tr.execute
-
-      # the different fare systems used by the two competing paths should equalise their total costs... 50/50 load
-      usage = calculate_usage([43.0, 45.0], -0.1) # see calculations.xls
-      usage.map! {|u| u * 10 }
-      check_load_based_on_usage(usage)
-    }
+    # the different fare systems used by the two competing paths should equalise their total costs... 50/50 load
+    usage = calculate_usage([43.0, 45.0], -0.1) # see calculations.xls
+    usage.map! {|u| u * 10 }
+    check_load_based_on_usage(usage)
   end
 
   def test_chaining
     # define the fare systems
     set_fare_system({3=>1, 4=>1, 5=>2}) # distance, distance, time
 
-    assert_nothing_raised() {
+    # schedule based properties
+    @tr.scheduleStartTime = @timePeriods.first
+    @tr.scheduleDurations = [5]
+    @tr.logitParameters  = [0.1]
 
-      # schedule based properties
-      @tr.scheduleStartTime = @timePeriods.first
-      @tr.scheduleDurations = [5]
-      @tr.logitParameters  = [0.1]
+    # EXECUTE!!
+    @tr.execute
 
-      # EXECUTE!!
-      @tr.execute
-
-      usage = calculate_usage([42.25, 42.5], -0.1) # see calculations.xls
-      usage.map! {|u| u * 10 }
-      check_load_based_on_usage(usage)
-    }
+    usage = calculate_usage([42.25, 42.5], -0.1) # see calculations.xls
+    usage.map! {|u| u * 10 }
+    check_load_based_on_usage(usage)
   end
 
   def test_zone_type
@@ -101,45 +96,39 @@ class TC_fares < OtTestCase
     # define the fare systems
     set_fare_system({3=>3, 4=>3, 5=>3}) # all stop type - based
 
-    assert_nothing_raised() {
+    # schedule based properties
+    @tr.scheduleStartTime = @timePeriods.first
+    @tr.scheduleDurations = [5]
+    @tr.logitParameters  = [0.1]
 
-      # schedule based properties
-      @tr.scheduleStartTime = @timePeriods.first
-      @tr.scheduleDurations = [5]
-      @tr.logitParameters  = [0.1]
+    # EXECUTE!!
+    @tr.execute
 
-      # EXECUTE!!
-      @tr.execute
+    # Check the output loads
+    usage = calculate_usage([42.0, 40.0], -0.1) # see calculations.xls
+    check_load_based_on_usage(usage.map! {|u| u * 10 } )
 
-      # Check the output loads
-      usage = calculate_usage([42.0, 40.0], -0.1) # see calculations.xls
-      check_load_based_on_usage(usage.map! {|u| u * 10 } )
-
-    }
   end
 
   def test_stop_to_stop
     # define the fare systems
     set_fare_system({3=>4, 4=>4, 5=>4}) # stop to stop - based
 
-    assert_nothing_raised() {
+    # schedule based properties
+    @tr.scheduleStartTime = @timePeriods.first
+    @tr.scheduleDurations = [5]
+    @tr.logitParameters  = [0.1]
+    @tr.skimMatrix = [1,1,1,1,[1,2,3,4,5,6,7],1]
 
-      # schedule based properties
-      @tr.scheduleStartTime = @timePeriods.first
-      @tr.scheduleDurations = [5]
-      @tr.logitParameters  = [0.1]
-      @tr.skimMatrix = [1,1,1,1,[1,2,3,4,5,6,7],1]
+    # EXECUTE!!
+    @tr.execute
 
-      # EXECUTE!!
-      @tr.execute
+    # Check the output loads
+    usage = calculate_usage([42.5, 40.5], -0.1) # see calculations.xls
+    check_load_based_on_usage(usage.map! {|u| u * 10 } )
 
-      # Check the output loads
-      usage = calculate_usage([42.5, 40.5], -0.1) # see calculations.xls
-      check_load_based_on_usage(usage.map! {|u| u * 10 } )
-
-      # Check the skims for fares
-      check_skim_matrix_contents([1,1,10050,1,6,1], {[1,2]=>1.5}, "Fares Skim")
-    }
+    # Check the skims for fares
+    check_skim_matrix_contents([1,1,10050,1,6,1], {[1,2]=>1.5}, "Fares Skim")
   end
 
   def test_stoptype_to_stoptype
@@ -148,24 +137,21 @@ class TC_fares < OtTestCase
     set_fare_system({3=>5, 4=>5, 5=>5}) #  stoptype to stoptype - based
     [1,2,3,4].each { |s|  set_stop_zone(s,s)     }
 
-    assert_nothing_raised() {
+    # schedule based properties
+    @tr.scheduleStartTime = @timePeriods.first
+    @tr.scheduleDurations = [5]
+    @tr.logitParameters  = [0.1]
+    @tr.skimMatrix = [1,1,1,1,[1,2,3,4,5,6,7],1]
 
-      # schedule based properties
-      @tr.scheduleStartTime = @timePeriods.first
-      @tr.scheduleDurations = [5]
-      @tr.logitParameters  = [0.1]
-      @tr.skimMatrix = [1,1,1,1,[1,2,3,4,5,6,7],1]
+    # EXECUTE!!
+    @tr.execute
 
-      # EXECUTE!!
-      @tr.execute
+    # Check the output loads
+    usage = calculate_usage([43.0, 41.0], -0.1) # see calculations.xls
+    check_load_based_on_usage(usage.map! {|u| u * 10 } )
 
-      # Check the output loads
-      usage = calculate_usage([43.0, 41.0], -0.1) # see calculations.xls
-      check_load_based_on_usage(usage.map! {|u| u * 10 } )
-
-      # Check the skims for fares
-      check_skim_matrix_contents([1,1,10050,1,6,1], {[1,2]=>2.0}, "Fares Skim")
-    }
+    # Check the skims for fares
+    check_skim_matrix_contents([1,1,10050,1,6,1], {[1,2]=>2.0}, "Fares Skim")
   end
 
   def test_missing_fareZone
