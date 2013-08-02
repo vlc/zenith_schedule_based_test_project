68d1ab65c33c1ef61b9ea4decb055fb388592946
jamie.cook <jamie.cook@1d98dbbe-d728-0410-a43a-8c3833854064>
2013-08-02 06:54:24 +0000

    - break out some of the boiler plate from the tests

---
+++ spec/ScheduleTestProject/jobs/ot_schedule_test_case.rb
@@ -0,0 +1,17 @@
+class OtScheduleTestCase < OtTestCase
+
+  # This is a convenience alias for when we are checking link5 outputs
+  alias :t :get_timetable_offset_time
+
+  def setup
+    clearOutputTables
+    @all_time_to_single_period_aggregation = { get_timetable_offset_time(0)..get_timetable_offset_time(110) => 2 }.to_a
+    @tr = OtTransit.new
+    @timePeriods = [get_timetable_offset_time(50)]
+    @timePeriods.each { |t| create_matrix([1,30,t,1,1,1], [[1,2,10]]) }
+    @tr.loadMatricesFromSkimCube = true
+    @tr.odMatrix = [1,30,@timePeriods,1,1,1]
+    super
+  end
+
+end
